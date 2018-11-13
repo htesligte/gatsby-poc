b9f378b7619590e8628814207da5fa1c537025ec
Henk te Sligte <htesligte@gmail.com>
2018-11-13 13:37:03 +0100

    fix

--- src/services/auth.js
+++ src/services/auth.js
@@ -34,11 +34,3 @@ export const logout = callback => {
     setUser({})
     callback()
 }
-
-/*
-CFPAT-c194da9a04a96c1ebdaf0fb0b0419c82507fe53b79efadf2a34cfa66fa1623fe
-
-xkbbyf0hrrr0
-content delivery api ce9dab6e5f51dcdd20a2545e7d7ebee2d6869f1962cc35c20e2f4f7b2954cf47
-content preview api 5b9723fbb740248c9a1f56403726f114075308f6115f8dea596c216c38b4f100
-*/
