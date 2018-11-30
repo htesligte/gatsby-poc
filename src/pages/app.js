8e3969159722c3f72b4ac1c68bfbf7d6335d2614
Henk te Sligte <htesligte@gmail.com>
2018-11-30 15:00:50 +0100

    reverted router back to reach-router because react-router breaks the gatsby build

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,23 +1,21 @@
 import React from "react"
-import { Router, Route } from "react-router-dom"
+import { Router } from "@reach/router"
 import Layout from "../components/layout"
+import PrivateRoute from "../components/privateRoute"
+import Profile from "../components/profile"
+import Login from "../components/login"
 import Blogs from "../components/blogs"
-import createBrowserHistory from "history/createBrowserHistory"
-
-const history = createBrowserHistory();
-
-history.listen(() => {
-    console.log("Yo.");
-})
+import Orders from "../components/orders"
 
 const App = () => (
-        <Layout>
-                <Router history={history}>
-                    <>
-                        <Route path="/app/blogs" component={Blogs} />
-                    </>
-                </Router>
-        </Layout>
+    <Layout>
+        <Router>
+            <PrivateRoute path="/app/profile" component={Profile} />
+            <Login path="/app/login" />
+            <Blogs path="/app/blogs" />
+            <Orders path="/app/orders" />
+        </Router>
+    </Layout>
 )
 
 export default App
